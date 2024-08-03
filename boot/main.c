d9c7af280382e8bb253fa272f0d20337108277bd
Jinsong <njs145@naver.com>
2024-08-03 17:33:28 +0900

    add Timer
    main.c
    1. add timer test app
    
    Regs.c
    1. add Timer reg
    
    Timer.c
    1. add Timer driver
    
    MemoryMap.h
    1. modify stack name
    
    stdlib.c
    1. add delay

--- boot/main.c
+++ boot/main.c
@@ -1,17 +1,15 @@
 #include "stdint.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdbool.h"
-
 #include "HalUART.h"
+#include "stdio.h"
+#include "stdbool.h"
 #include "HalInterrupt.h"
+#include "HalTimer.h"
 
 static void Hw_init(void);
 static void Printf_test(void);
 
 void main(void)
 {
-    uint8_t data;
     Hw_init();
 
     uint32_t i = 100;
@@ -19,20 +17,25 @@ void main(void)
     {
         Hal_uart_put_char('N');
     }
-    Hal_uart_put_char('\n');
 
-    putstr("Hello World! \n");
+    Hal_uart_put_char('\n');
+    
+    putstr("Hello World!\n");
 
     Printf_test();
 
-    while(true);
-    
+    while(true)
+    {
+        debug_printf("Current Count : %u \n",Hal_timer_get_1ms_counter());
+        delay(1000);
+    };
 }
 
 static void Hw_init(void)
 {
     Hal_interrupt_init();
     Hal_uart_init();
+    Hal_timer_init();
 }
 
 static void Printf_test(void)
@@ -40,6 +43,7 @@ static void Printf_test(void)
     char* str = "printf pointer test";
     char* nullptr = 0;
     uint32_t i = 5;
+    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
 
     debug_printf("%s\n", "Hello printf");
     debug_printf("output string pointer: %s\n", str);
@@ -47,4 +51,5 @@ static void Printf_test(void)
     debug_printf("%u = 5\n", i);
     debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
     debug_printf("print zero %u\n", 0);
+    debug_printf("SYSCTRL0 = %u\n", *sysctrl0);
 }
--- hal/HalUART.h
+++ hal/HalUART.h
@@ -1,5 +1,5 @@
 #ifndef HAL_HALUART_H_
-#define HAL_HALUART_H__ 
+#define HAL_HALUART_H_ 
 
 void Hal_uart_init(void);
 void Hal_uart_put_char(uint8_t ch);
